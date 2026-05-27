bfd23b7fe1fe447ad4b8cdbe92ecaa4ad0b3714e
it8fun <trey.wolf07@gmail.com>
2026-05-27 12:03:42 -0500

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -20,3 +20,37 @@ for (var i = 0; i < skills.length; i++){
     skillsList.appendChild(skill);
 }
 
+const messageForm = document.forms["leave_message"];
+
+messageForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+
+    console.log(usersName, usersEmail, usersMessage);
+
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+
+    newMessage.innerHTML = `
+    <a href="mailto:${usersEmail}">${usersName}</a>
+    <br>
+    <span>${usersMessage}</span>
+    <br>
+    `;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', function(){
+        const entry = removeButton.parentNode;
+        entry.remove();
+    })
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    messageForm.reset();
+})
